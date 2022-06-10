5313b3a0a66ffbb4db550c92dcd566d8d22fb3f2
Vladimir Turcan <vladimir.turcan@sumup.com>
2022-06-10 10:49:55 +0300

    feat: add serverless func

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const path = require("path");
+const fs = require("fs");
+
+let cachedValue = null;
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  logFiles();
+
+  if (!cachedValue) {
+    cachedValue = Math.random().toFixed(2);
+  }
+
+  res.status(200).json({ name: "John Doe", value: cachedValue });
+}
+
+function logFiles() {
+  fs.readdir(__dirname, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      console.log(file);
+    });
+  });
 }
